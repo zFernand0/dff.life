866b5fa184520aa68ede46f8266d73bf4229cefc
Fernando <fernando@rijo.dev>
2025-12-06 11:38:17 -0500

    chore: fix build issues
    Signed-off-by: Fernando <fernando@rijo.dev>

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+const dir = __dirname + '/src/';
 export default defineConfig({
     build: {
         outDir: 'docs',
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                about: resolve(__dirname, 'about.html'),
-                privacy: resolve(__dirname, 'privacy.html'),
-                terms: resolve(__dirname, 'terms.html'),
+                main: resolve(dir, 'index.html'),
+                about: resolve(dir, 'about.html'),
+                privacy: resolve(dir, 'privacy.html'),
+                terms: resolve(dir, 'terms.html'),
+                cc_mgmt_guide: resolve(dir, 'cc_mgmt_guide.html'),
+                cc_pmt_example: resolve(dir, 'cc_pmt_example.html'),
+                credit_score_explained: resolve(dir, 'credit_score_explained.html'),
             },
         },
     },
