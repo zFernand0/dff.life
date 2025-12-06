601d2da684d61387b169002def667c316022b516
Fernando <fernando@rijo.dev>
2025-12-06 12:26:15 -0500

    chore: minify built html
    Signed-off-by: Fernando <fernando@rijo.dev>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,6 +9,9 @@ const __dirname = dirname(__filename);
 export default defineConfig({
     root: 'src',
     publicDir: '../public',
+    plugins: [
+        ViteMinifyPlugin({}),
+    ],
     build: {
         outDir: '../docs',
         emptyOutDir: true,
